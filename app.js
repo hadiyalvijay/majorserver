5174996109977f79029dd6d99d12fb3f45b76ab2
Hadiyal Vijay <hadiyalvijay7777@gmail.com>
2025-02-04 11:28:18 +0530

    update commint

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const upload = multer({
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],  
+  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175'],  
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
